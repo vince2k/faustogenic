db8b4e74fd1e6110bbd1995d6d77477dade41bdc
Vincent de Cabissole <vince2k@protonmail.com>
2025-05-15 11:49:30 +0200

    activeAdmin setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { active_admin_controller? }
+
+  private
+
+  def active_admin_controller?
+    self.class < ActiveAdmin::BaseController
+  end
 end
